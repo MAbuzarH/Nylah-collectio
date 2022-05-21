eee3dbe2ebdc1ac12dabbc34537322923c748a13
MAbuzarH <103282484+MAbuzarH@users.noreply.github.com>
2022-05-21 14:49:01 +0500

    Add files via upload

---
+++ p.js
@@ -0,0 +1,29 @@
+// const radious=[3,1,2,4];
+// //for finding area of circule
+// const area=function(radious){
+// return Math.PI*radious*radious;
+// }
+// //for circumfarance
+// const circumfarance=function(radious){
+//     return 2*Math.PI*radious;
+//     }
+//     //for diyameter
+//  const diyameter=function(radious){
+//         return 2*radious;
+//         } 
+// const calculate=function(radious,logic){
+// const output=[];
+// for(i=0; i<radious.length; i++ ){
+//     output.push(logic(radious[i]));
+// }
+// return output;
+// }
+// console.log("area of a circle");
+// console.log(calculate(radious,area));
+// console.log("circumfarence of a circle");
+// console.log(calculate(radious,circumfarance));
+// console.log("diyameter of a circle");
+// console.log(calculate(radious,diyameter));
+let str="apple,banan,cst,gols";
+let b=str.slice(7.14);
+console.log(b);
